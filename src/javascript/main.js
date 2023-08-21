225aca71f829857f130bc10ba7ea7fd82fbe6e1f
birajnior <ubiratanbernardo@gmail.com>
2023-08-21 10:46:26 -0300

    Adicionando validações

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,7 +1,33 @@
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento != null && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    alert("Elemento não encontrado ou seletor inválido!!!");
+  }
+}
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.addEventListener("click", () => {
+    tocaSom(idAudio);
+  });
+
+  tecla.onkeydown = function (event) {
+    if (event.code === "Space" || event.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
 
 // let contador = 0;
@@ -15,13 +41,3 @@ function tocaSom(idElementoAudio) {
 //   });
 //   contador = contador + 1;
 // }
-
-for (let i = 0; i < listaDeTeclas.length; i++) {
-  const tecla = listaDeTeclas[i];
-  const instrumento = tecla.classList[1];
-  const idAudio = `#som_${instrumento}`;
-
-  tecla.addEventListener("click", () => {
-    tocaSom(idAudio);
-  });
-}
